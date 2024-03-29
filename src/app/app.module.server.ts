9309e00b4cf26201ab44c08885231affbc04dd3a
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-03-29 11:52:16 +0100

    Add NGPrime

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -4,11 +4,11 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+    imports: [AppModule, ServerModule, BrowserModule, BrowserAnimationsModule],
+    bootstrap: [AppComponent],
 })
 export class AppServerModule {}

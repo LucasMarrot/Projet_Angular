367ab74cf43fcb9d692ced12837121d929c57e4a
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2024-04-02 11:04:15 +0200

    Add chaudiere card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
 import { HeaderComponent } from './components/base/header/header.component';
 import { FooterComponent } from './components/base/footer/footer.component';
+import { ChaudiereCardComponent } from './components/chaudieres/chaudiere-card/chaudiere-card.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule, ButtonModule, MenubarModule],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        ChaudiereCardComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ButtonModule,
+        MenubarModule,
+        CardModule,
+    ],
     providers: [provideClientHydration()],
     bootstrap: [AppComponent],
 })
---
+++ src/app/components/chaudieres/chaudiere-card/chaudiere-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chaudiere-card',
+  templateUrl: './chaudiere-card.component.html',
+  styleUrl: './chaudiere-card.component.scss'
+})
+export class ChaudiereCardComponent {
+
+}
